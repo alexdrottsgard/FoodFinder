961e88b8cb2e244c1adc206c73d8777093b35df3
filipheidfors <filipheidfors@hotmail.com>
2017-03-31 14:44:03 +0200

    Skrivit metod getData() och streamToString()

--- FoodFinder/app/src/main/java/se/group14/foodfinder/Restaurant.java
+++ FoodFinder/app/src/main/java/se/group14/foodfinder/Restaurant.java
@@ -15,6 +15,33 @@ public class Restaurant {
     private int price;
     private String category;
     private String hours;
+    private int distance;
+
+    public Restaurant(String name, String address, String phone, double rating, double latitude, double longitude, String website, int price, String category, String hours) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.rating = rating;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.website = website;
+        this.price = price;
+        this.category = category;
+        this.hours = hours;
+
+    }
+
+    public Restaurant() {
+
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 
     public String getAddress() {
         return address;
@@ -22,6 +49,46 @@ public class Restaurant {
 
     public String getPhone() {
         return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setHours(String hours) {
+        this.hours = hours;
     }
 
     public double getRating() {
@@ -57,17 +124,5 @@ public class Restaurant {
 
     }
 
-    public Restaurant(String name, String address, String phone, double rating, double latitude, double longitude, String website, int price, String category, String hours) {
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-        this.rating = rating;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.website = website;
-        this.price = price;
-        this.category = category;
-        this.hours = hours;
 
-    }
 }
--- FoodFinder/app/src/main/java/se/group14/foodfinder/SearchController.java
+++ FoodFinder/app/src/main/java/se/group14/foodfinder/SearchController.java
@@ -1,16 +1,115 @@
 package se.group14.foodfinder;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 /**
  * Created by AlexanderJD on 2017-03-17.
  */
 
 public class SearchController {
     private int distance, price;
-    private Restaurant[] restaurants;
+    private static final String CLIEND_ID = "QTBTJY4EUWO0TROZGBRZ4I1YZN51DCG4UMM11IBUCWFLHVXF";
+    private static final String CLIENT_SECRET = "EX42ZK4A210FHPKT5SK1VXHJCDNAEOXYZUVECOEU1PFNIBEB";
+    private ArrayList<Restaurant> restaurants;
 
     public SearchController(int distance, int price) {
         this.distance=distance;
         this.price=price;
         System.out.println(distance + " " + price);
+        getData();
+    }
+
+    public void getData() {
+        try {
+            URL url = new URL("https://api.foursquare.com/v2/venues/search?ll=40.7,-74&section=food&radius="+distance+"&client_id="+CLIEND_ID+"&client_secret="+CLIENT_SECRET+"&v=20170331");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            String response		= streamToString(urlConnection.getInputStream());
+            JSONObject jsonObj 	= (JSONObject) new JSONTokener(response).nextValue();
+
+            JSONArray groups	= (JSONArray) jsonObj.getJSONObject("response").getJSONArray("groups");
+
+            int length			= groups.length();
+
+            if (length > 0) {
+                for (int i = 0; i < length; i++) {
+                    JSONObject group 	= (JSONObject) groups.get(i);
+                    JSONArray items 	= (JSONArray) group.getJSONArray("items");
+
+                    int ilength 		= items.length();
+
+                    for (int j = 0; j < ilength; j++) {
+                        JSONObject item = (JSONObject) items.get(j);
+
+                        Restaurant restaurant 	= new Restaurant();
+
+                        restaurant.setName(item.getString("name"));
+
+                        JSONObject location = (JSONObject) item.getJSONObject("location");
+
+                        restaurant.setLatitude(Double.valueOf(location.getString("lat")));
+                        restaurant.setLatitude(Double.valueOf(location.getString("lng")));
+
+                        restaurant.setAddress(location.getString("address"));
+                        restaurant.setDistance(location.getInt("distance"));
+
+
+                        //JSONObject category = (JSONObject) item.getJSONObject("categorie");
+                        //restaurant.setCategory(lo);
+
+                        restaurants.add(restaurant);
+
+                        System.out.println(restaurants.toArray());
+                    }
+                }
+            }
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String streamToString(InputStream is) throws IOException {
+        String str  = "";
+
+        if (is != null) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            try {
+                BufferedReader reader 	= new BufferedReader(new InputStreamReader(is));
+
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                reader.close();
+            } finally {
+                is.close();
+            }
+
+            str = sb.toString();
+        }
+
+        return str;
     }
 }
