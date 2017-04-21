b3b334116e44984d2270fc246f4a0e7cd2c0a70a
filipheidfors <filipheidfors@hotmail.com>
2017-04-21 14:07:02 +0200

    Fixat så att mapviewn inte buggar längre

--- FoodFinder/app/src/main/java/se/group14/foodfinder/ResultMapView.java
+++ FoodFinder/app/src/main/java/se/group14/foodfinder/ResultMapView.java
@@ -2,6 +2,7 @@ package se.group14.foodfinder;
 
 //import android.app.Fragment;
 import android.app.Activity;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -26,25 +28,38 @@ public class ResultMapView extends Fragment implements OnMapReadyCallback{
     private ArrayList<Restaurant> restaurants;
     private MapView mapView;
     private GoogleMap mGoogleMap;
+    private View view;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.result_map_view, container, false);
-
-        mapView = (MapView) rootView.findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
+        view = inflater.inflate(R.layout.result_map_view, container, false);
         //addMarkers();
-        return rootView;
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = (MapView) view.findViewById(R.id.mapView);
+
+        if(mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
         mGoogleMap = googleMap;
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
 
         LatLng pos = new LatLng(55.609069,12.99467);
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 12));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 15));
         addMarkers();
     }
 
