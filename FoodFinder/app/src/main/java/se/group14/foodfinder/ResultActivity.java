f70fed717a1fbbb5352452b92d9a4360ba591731
filipheidfors <filipheidfors@hotmail.com>
2017-04-21 19:59:19 +0200

    Tagit bort toolbar i result activity

--- FoodFinder/app/src/main/java/se/group14/foodfinder/ResultActivity.java
+++ FoodFinder/app/src/main/java/se/group14/foodfinder/ResultActivity.java
@@ -67,8 +67,8 @@ public class ResultActivity extends AppCompatActivity {
         map = new ResultMapView();
         map.setArguments(restaurants,userPosition,this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
