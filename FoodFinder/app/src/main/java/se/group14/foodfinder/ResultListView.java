1f7e83c93a1ea5c36ec0e6690c4ffbda650c6768
drottsgard <aalexjd@gmail.com>
2017-05-02 14:09:53 +0200

    förändringar i restaurant list igen

--- FoodFinder/app/src/main/java/se/group14/foodfinder/ResultListView.java
+++ FoodFinder/app/src/main/java/se/group14/foodfinder/ResultListView.java
@@ -82,11 +82,14 @@ public class ResultListView extends Fragment implements Comparator<Restaurant> {
             View view = View.inflate(mContext, R.layout.restaurant_list_data, null);
             TextView restaurantName = (TextView) view.findViewById(R.id.restaurantName);
             TextView distance = (TextView) view.findViewById(R.id.distance);
+            TextView priceClass = (TextView) view.findViewById(R.id.priceClass);
             TextView rating = (TextView) view.findViewById(R.id.rating);
+
 
             restaurantName.setText(restaurants.get(position).getName());
             distance.setText(restaurants.get(position).getDistance() + " meter");
-            distance.setText("Prisklass: " + restaurants.get(position).getPrice());
+            priceClass.setText("Prisklass: " + restaurants.get(position).getPrice());
+            distance.setText("Avstånd: " + restaurants.get(position).getDistance());
             String ratingString = (restaurants.get(position).getRating() == 0.0) ? "?" : ""+restaurants.get(position).getRating();
             rating.setText(String.valueOf(ratingString + "/10"));
 
